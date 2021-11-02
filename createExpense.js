ee42c8aebe44423d6c0efbe9ee7ab44215142d02
developer-devendra <devendra@clutterbot.com>
2021-11-02 22:20:39 +0530

    added mutation createExpanse

---
+++ createExpense.js
@@ -0,0 +1,63 @@
+const AWS = require('aws-sdk')
+const dynamodb = new AWS.DynamoDB()
+const { v4: uuidv4 } = require('uuid');
+
+module.exports.handler = async (event) => {
+    const id = uuidv4()
+    const reciept = event.arguments.reciept
+    const date = event.arguments.date
+    const amount = event.arguments.amount
+    const categoryId = event.arguments.categoryId
+    const userId = event.arguments.userId
+
+    const params = {
+        Item: {
+            "id": {
+                S: id
+            },
+            "date": {
+                S: date
+            },
+            "amount": {
+                N: amount.toString()
+            },
+            "reciept": {
+                S: reciept
+            },
+            "categoryId": {
+                S: categoryId
+            },
+            "userId": {
+                S: userId
+            }
+        },
+        ReturnConsumedCapacity: "TOTAL",
+        TableName: process.env.EXPENSE_TABLE_NAME
+    }
+
+    return dynamodb.putItem(params).promise()
+        .then(data => {
+            return {
+                data: {
+                    id,
+                    date,
+                    amount,
+                    reciept,
+                    categoryId,
+                    userId
+                },
+                description: "",
+                status: true
+
+            }
+        })
+        .catch(err => {
+            return {
+                data: {
+                },
+                description: err || "internal server error",
+                status: true
+
+            }
+        })
+};
